f4fa57157eb81441c82e82e0fdf4c66c54d85bde
Khlynin Roman Olegovich <roma.hlinin@yandex.ru>
2023-10-12 22:49:53 +0300

    Adding required microservices

---
+++ VTBCollaborativeAccount/VTBService/RequestSenders/VTBAuthRequestSender.cs
@@ -0,0 +1,11 @@
+﻿namespace VTBService.RequestSenders;
+
+public class VTBAuthRequestSender
+{
+    private HttpClient _client;
+    public VTBAuthRequestSender(HttpClient client)
+    {
+        _client = client;
+    }
+    
+}
